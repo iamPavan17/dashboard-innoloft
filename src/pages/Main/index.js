d105b4688cc4167c1b808e90ad2888a47192a153
Pavan S <iampavan05@gmail.com>
2023-04-22 00:26:29 +0530

    ✨ added some helper text > main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,17 @@
 import Text from "components/Text";
+import { Link } from "react-router-dom";
 import { Section } from "../styles";
+import { PRODUCT_VIEW_ROUTE } from "App/routes";
 
 export default function Main() {
   return (
     <Section>
       <Text textAlign="center" fontWeight="bold" fontSize="h1">
-        Hello 👋🏼, This is a Main Page.
+        Hello 👋🏼, This is Main Page.
+      </Text>
+      <Text textAlign="center" fontSize="h5">
+        To view the product details, visit to the product page from the above
+        App bar or Click <Link to={PRODUCT_VIEW_ROUTE}>here</Link>.
       </Text>
     </Section>
   );
